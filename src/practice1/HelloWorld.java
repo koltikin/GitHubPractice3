83ed790fe2702249c90ac79204078089181027db
zubeyde <zubeyde1014@gmail.com>
2023-07-08 19:09:20 +0300

    add4Int method added to HelloWorld class

--- src/practice1/HelloWorld.java
+++ src/practice1/HelloWorld.java
@@ -4,4 +4,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+
+    public static int add4Int(int a,int b,int c,int d){
+        return a+b+c+d;
+    }
 }
