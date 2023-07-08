7fffaad0e3b7ba76a045ce71f3a12f53dd0daeff
zubeyde <zubeyde1014@gmail.com>
2023-07-08 19:37:58 +0300

    add4Int method implemented in HelloWorld class

--- src/practice1/HelloWorld.java
+++ src/practice1/HelloWorld.java
@@ -3,6 +3,7 @@ package practice1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println(add3Int(3,4,5)+add4Int(4,7,8,6));
         System.out.println(add4Int(5,8,9,7));
         System.out.println(add3Int(3,8,9));
     }
